02fec0450aac6e77df012848452e52f0653b1b61
Danial <danil.malaev3@gmail.com>
2024-11-03 14:35:54 +0200

    Empty-Commit

--- lagoona/src/components/micro_components/SpecialOffers.tsx
+++ lagoona/src/components/micro_components/SpecialOffers.tsx
@@ -6,14 +6,14 @@ const SpecialOffers: React.FC = () => {
     return (
         <section>
             <div className="container">
-                <h2 className="special_h2">Спецпредложения</h2>
+                <h2 className="special_h2">Special Offers</h2>
                 <div className="special flex">
                     <div className="special_block flex">
-                        <SpecialOfferComponent title="Мальдивские острова" price="2500$" link="#" ArticleClassName="special_item maldivs" TitleClassName="special-item-title"/>
-                        <SpecialOfferComponent title="Остров Крит" price="1700$" link="#" ArticleClassName=" special_item crete" TitleClassName="special-item-title"/>
+                        <SpecialOfferComponent title="Maldivs" price="3800$" link="#" ArticleClassName="special_item maldivs" TitleClassName="special-item-title"/>
+                        <SpecialOfferComponent title="Crete Island" price="2100$" link="#" ArticleClassName=" special_item crete" TitleClassName="special-item-title"/>
                     </div>
                     <div className="special_block flex">
-                        <SpecialOfferComponent title="Номера категории люкс" price="400$" link="#" ArticleClassName="special_item_2 luxe" TitleClassName="special-item-title-2"/>
+                        <SpecialOfferComponent title="Luxury rooms" price="700$" link="#" ArticleClassName="special_item_2 luxe" TitleClassName="special-item-title-2"/>
                     </div>
                 </div>
             </div>
