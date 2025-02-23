feb9eb09223f148527e2d2f8eb975ec7cbd27404
Danial <danil.malaev3@gmail.com>
2025-02-23 22:53:34 +0200

    scroll-indicator implementation

--- lagoona/src/App.tsx
+++ lagoona/src/App.tsx
@@ -1,14 +1,34 @@
 import Header from "./components/macro_components/Header";
 import MainPageComponent from "./components/macro_components/MainPageComponent";
 import Footer from "./components/macro_components/Footer";
+import { motion, useScroll } from "framer-motion";
 
 function App() {
+
+  const { scrollYProgress } = useScroll();
+
   return (
-    <div className='content-page'>
-      <Header />
-      <MainPageComponent />
-      <Footer />
-    </div>
+    <>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 5,
+          originX: 0,
+          backgroundColor: "#f0c060"
+        }}
+      />
+
+      <div className='content-page'>
+        <Header />
+        <MainPageComponent />
+        <Footer />
+      </div>
+    </>
   );
 }
 export default App;
