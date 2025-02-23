ef77b419737d282f98d165ae5eafce0e68ef9c13
Danial <danil.malaev3@gmail.com>
2025-02-23 23:04:03 +0200

    the sun-element now have interactive x-y  interection

--- lagoona/src/components/micro_components/FooterComponent.tsx
+++ lagoona/src/components/micro_components/FooterComponent.tsx
@@ -1,8 +1,42 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
 import Social from "./Social";
 import facebook from "../../assets/facebook.svg";
 import instagram from "../../assets/instagram.svg";
 import whatsup from "../../assets/whatsup.svg";
 import YellowLinks from "./YellowLinks";
+
+function Line({ direction, activeDirection }: { direction: "x" | "y", activeDirection: "x" | "y" | null }) {
+    return (
+        <motion.div
+            initial={false}
+            animate={{ opacity: activeDirection === direction ? 1 : 0.3 }}
+            transition={{ duration: 0.1 }}
+            style={{ ...line, rotate: direction === "y" ? 90 : 0 }}
+        />
+    );
+}
+
+function Drag() {
+    const [activeDirection, setActiveDirection] = useState<"x" | "y" | null>(null);
+
+    return (
+        <>
+            <Line direction="y" activeDirection={activeDirection} />
+            <motion.div
+                drag
+                dragDirectionLock
+                onDirectionLock={(direction) => setActiveDirection(direction)}
+                onDragEnd={() => setActiveDirection(null)}
+                dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                dragTransition={{ bounceStiffness: 500, bounceDamping: 15 }}
+                dragElastic={0.2}
+                whileDrag={{ cursor: "grabbing" }}
+                className="sun-image"
+            />
+        </>
+    );
+}
 
 const Footer = () => {
     return (
@@ -39,8 +73,8 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="block-with-image">
-                    <div className="sun-image"></div>
+                <div className="block-with-image sun">
+                    <Drag/>
                 </div>
 
                 <div className="flex social">
@@ -51,6 +85,10 @@ const Footer = () => {
             </div>
         </div>
     );
+};
+
+const line: React.CSSProperties = {
+    height: 187,
 };
 
 export default Footer;
